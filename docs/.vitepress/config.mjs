40f24da102765b0ba6d1d7e6e5d1302ea4801f4c
Sourabh Choure <sourabhchoure98@gmail.com>
2024-03-13 16:03:26 -0400

    change title template

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "thesawrub",
-  titleTemplate: 'sawrub',
+  titleTemplate: 'thesawrub',
   description: "Thesaurus for Sourabh",
   themeConfig: {
     search: {
