4c64c8cefc9f54662d96ed1d9c9b5e2b2e07a3b0
Sourabh Choure <sourabhchoure98@gmail.com>
2024-03-13 16:10:29 -0400

    add favicon

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,16 +3,16 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "thesawrub",
-  titleTemplate: 'thesawrub',
+  titleTemplate: "thesawrub",
   description: "Thesaurus for Sourabh",
   themeConfig: {
     search: {
-      provider: 'local'
+      provider: "local",
     },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "Home", link: "/" },
-      { text: "About Me", link: "/about-me/", activeMatch: '/about-me/' },
+      { text: "About Me", link: "/about-me/", activeMatch: "/about-me/" },
     ],
     logo: { src: "/bgpme.png", width: 24, height: 24 },
 
@@ -35,6 +35,35 @@ export default defineConfig({
       { icon: "linkedin", link: "https://linkedin.com/in/sjchoure" },
       { icon: "github", link: "https://github.com/sjchoure" },
       { icon: "instagram", link: "https://instagram.com/sjchoure" },
+    ],
+    head: [
+      [
+        "link",
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+      ],
+      [
+        "link",
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+      ],
+      [
+        "link",
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+      ],
+      ["link", { rel: "manifest", href: "/site.webmanifest" }],
     ],
     footer: {
       message: "Released under the MIT License.",
