50883436c5541cabcee133d425ef7fd144172f5d
Sourabh Choure <sourabhchoure98@gmail.com>
2024-03-13 16:29:10 -0400

    add head

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -5,6 +5,49 @@ export default defineConfig({
   title: "thesawrub",
   titleTemplate: "thesawrub",
   description: "Thesaurus for Sourabh",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    ["meta", { property: "og:url", content: "https://thesawrub.com" }],
+    ["meta", { property: "og:type", content: "website" }],
+    ["meta", { property: "og:title", content: "thesawrub" }],
+    ["meta", { property: "og:description", content: "Thesaurus for Sourabh" }],
+    ["meta", { property: "og:image", content: "/thesawrub.png" }],
+    ["meta", { property: "twitter:card", content: "summary_large_image" }],
+    ["meta", { property: "twitter:domain", content: "thesawrub.com" }],
+    ["meta", { property: "twitter:url", content: "https://thesawrub.com" }],
+    ["meta", { property: "twitter:title", content: "thesawrub" }],
+    [
+      "meta",
+      { property: "twitter:description", content: "Thesaurus for Sourabh" },
+    ],
+    ["meta", { property: "twitter:image", content: "/thesawrub.png" }],
+  ],
   themeConfig: {
     search: {
       provider: "local",
@@ -35,52 +78,6 @@ export default defineConfig({
       { icon: "linkedin", link: "https://linkedin.com/in/sjchoure" },
       { icon: "github", link: "https://github.com/sjchoure" },
       { icon: "instagram", link: "https://instagram.com/sjchoure" },
-    ],
-    head: [
-      [
-        "link",
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
-          href: "/favicon-32x32.png",
-        },
-      ],
-      [
-        "link",
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          href: "/favicon-16x16.png",
-        },
-      ],
-      [
-        "link",
-        {
-          rel: "apple-touch-icon",
-          sizes: "180x180",
-          href: "/apple-touch-icon.png",
-        },
-      ],
-      ["link", { rel: "manifest", href: "/site.webmanifest" }],
-      ["meta", { property: "og:url", content: "https://thesawrub.com" }],
-      ["meta", { property: "og:type", content: "website" }],
-      ["meta", { property: "og:title", content: "thesawrub" }],
-      [
-        "meta",
-        { property: "og:description", content: "Thesaurus for Sourabh" },
-      ],
-      ["meta", { property: "og:image", content: "/thesawrub.png" }],
-      ["meta", { property: "twitter:card", content: "summary_large_image" }],
-      ["meta", { property: "twitter:domain", content: "thesawrub.com" }],
-      ["meta", { property: "twitter:url", content: "https://thesawrub.com" }],
-      ["meta", { property: "twitter:title", content: "thesawrub" }],
-      [
-        "meta",
-        { property: "twitter:description", content: "Thesaurus for Sourabh" },
-      ],
-      ["meta", { property: "twitter:image", content: "/thesawrub.png" }],
     ],
     footer: {
       message: "Released under the MIT License.",
